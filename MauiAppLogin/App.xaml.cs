bd5577f89d4ddb94348be39e13b99fc6b93956b3
Celso Gonçalves <goncl7@gmail.com>
2025-05-03 18:52:52 -0300

    Imprementação da pagina de Login e Protegida.

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppLogin
+﻿using Microsoft.Maui.Storage;
+using Microsoft.Maui.Dispatching;
+
+namespace MauiAppLogin
 {
     public partial class App : Application
     {
@@ -6,18 +9,43 @@
         {
             InitializeComponent();
 
-            MainPage = new Login();
+            // Página temporária com indicador de carregamento
+            MainPage = new ContentPage
+            {
+                Content = new ActivityIndicator
+                {
+                    IsRunning = true,
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center
+                }
+            };
+
+            // Executa na thread principal
+            MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                try
+                {
+                    var usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                    if (usuario_logado == null)
+                        MainPage = new Login();
+                    else
+                        MainPage = new Protegida();
+                }
+                catch (Exception ex)
+                {
+                    await MainPage.DisplayAlert("Erro", $"Falha ao acessar dados seguros: {ex.Message}", "Fechar");
+                    MainPage = new Login();
+                }
+            });
         }
 
         protected override Window CreateWindow(IActivationState activationState)
         {
             var window = base.CreateWindow(activationState);
-
             window.Width = 400;
             window.Height = 600;
-
             return window;
         }
-
     }
 }
--- MauiAppLogin/Protegida.xaml.cs
+++ MauiAppLogin/Protegida.xaml.cs
@@ -2,8 +2,34 @@ namespace MauiAppLogin;
 
 public partial class Protegida : ContentPage
 {
-	public Protegida()
-	{
-		InitializeComponent();
-	}
+    public Protegida()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        try
+        {
+            var usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+            lbl_boasvindas.Text = $"Bem-vindo(a) {usuario_logado}";
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Erro ao carregar dados: {ex.Message}", "OK");
+        }
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        bool confirmacao = await DisplayAlert("Tem certeza?", "Sair do App?", "Sim", "Não");
+
+        if (confirmacao)
+        {
+            SecureStorage.Default.Remove("usuario_logado");
+            App.Current.MainPage = new Login();
+        }
+    }
 }
